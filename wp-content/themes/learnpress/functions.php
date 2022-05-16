702e52c4a878c71c1512e26445eb9bfdfc089893
tunngnt <tungnt@icd-vn.com>
2022-05-16 17:03:13 +0700

    tungnt

--- wp-content/themes/learnpress/functions.php
+++ wp-content/themes/learnpress/functions.php
@@ -186,17 +186,14 @@ function refresh_cart_count( $fragments ){
     ?></span>
     <?php
      $fragments['#m-cart_num'] = ob_get_clean();
-
     return $fragments;
 }
 //auto update cart number item by ajax
 
 add_filter( 'woocommerce_checkout_fields' , 'custom_remove_woo_checkout_fields' );
 // ! auto update cart number item by ajax
-
 // ! custom_remove_woo_checkout_fields
 function custom_remove_woo_checkout_fields( $fields ) {
-
     // remove billing fields
     // unset($fields['billing']['billing_first_name']);
     unset($fields['billing']['billing_last_name']);
@@ -207,9 +204,6 @@ function custom_remove_woo_checkout_fields( $fields ) {
     unset($fields['billing']['billing_postcode']);
     unset($fields['billing']['billing_country']);
     unset($fields['billing']['billing_state']);
-    // unset($fields['billing']['billing_phone']);
-    // unset($fields['billing']['billing_email']);
-
     // remove shipping fields
     unset($fields['shipping']['shipping_first_name']);
     unset($fields['shipping']['shipping_last_name']);
