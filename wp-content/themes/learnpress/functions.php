7781826457c6e5c381c5c87954d4e2e074b0fd7b
tungnt <nguyenthanhtung74119@gmail.com>
2022-05-16 23:57:56 +0700

    tungt

--- wp-content/themes/learnpress/functions.php
+++ wp-content/themes/learnpress/functions.php
@@ -236,3 +236,46 @@ function bbloomer_thankyou_change_order_status( $order_id ){
 	 }
 }
 // !control order status if vnpay succes pay
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Ebook.
+	 */
+
+	$labels = [
+		"name" => __( "Ebook", "custom-post-type-ui" ),
+		"singular_name" => __( "Ebook", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Ebook", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "ebook", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-admin-page",
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "ebook", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
