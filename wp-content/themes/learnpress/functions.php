1d9753328b355126ebbf3db22d62d0a9ef62f2b2
tungnt <nguyenthanhtung74119@gmail.com>
2022-05-23 07:31:36 +0700

    tungnt

--- wp-content/themes/learnpress/functions.php
+++ wp-content/themes/learnpress/functions.php
@@ -290,3 +290,31 @@ function no_self_ping( &$links ) {
 add_action( 'pre_ping', 'no_self_ping' );
 add_filter('xmlrpc_enabled', '__return_false');
 #!Disable Self Pingbacks in WordPress Using Plugins
+
+#Track Post Views
+function getCrunchifyPostViews($postID){
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+        return "0 View";
+    }
+    return $count.' Views';
+}
+ 
+function setCrunchifyPostViews($postID) {
+    $count_key = 'post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+ 
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+#!Track Post Views
--- wp-content/themes/learnpress/page-blog.php
+++ wp-content/themes/learnpress/page-blog.php
@@ -13,30 +13,65 @@
  */
 
 get_header();
+global $post;
+$paged = get_query_var('paged') ? get_query_var('paged') : 1;
+$myposts = get_posts( array(
+  'post_type' => array('post'),
+  'post_status' => 'publish',
+  'orderby' => 'ID',
+  'order' => 'DESC',
+) );
+$bigpost = get_posts( array(
+  'post_type' => array('post'),
+  'post_status' => 'publish',
+  'orderby' => 'ID',
+  'posts_per_page' => 1,  
+  'order' => 'DESC',
+) );
+$i = 0;
 ?>
       <main class="l-main">
         <div class="l-container">
           <div class="c-banner">
             <div class="c-banner_inner">
-              <div class="c-banner_big">
-                <a class="c-banner_img" href="#">
-                  <img src="https://picsum.photos/800/478" alt="" width="816" height="378" srcset="">
-                </a>
-                <div class="c-banner_content">
-                  <a class="tag_a" href="#">
-                  <div class="c-banner_category">Marketing Strage</div>
-                  <h3 class="c-banner_ttl">5 Steps to Create an Outstanding Marketing Plan [Free Templates]</h3>
-                  <p class="c-banner_time">MARKETING | 15 MIN READ</p>
-                  </a>
-                </div>
-              </div>
+              <?php if ( $bigpost ) 
+                { foreach ( $bigpost as $postbig ) : setup_postdata( $postbig ); ?>
+                    <?php  var_dump(get_the_post_thumbnail_url($postbig->ID)); ?>
+                    <div class="c-banner_big">
+                      <a class="c-banner_img" href="<?php the_permalink($postbig->ID); ?>">
+                        <img src="" alt="" width="816" height="378" srcset="">
+                      </a>
+                      <div class="c-banner_content">
+                        <a class="tag_a" href="<?php the_permalink($postbig->ID); ?>">
+                        <div class="c-banner_category"><?php echo get_the_category($postbig->ID)[0]->name; ?>  </div>
+                        <h3 class="c-banner_ttl"><?php echo($postbig->post_title); ?></h3>
+                        <p class="c-banner_time"><?php echo get_the_category($postbig->ID)[0]->name; ?>  | <?php the_field('time_read', $postbig->ID); ?> MIN READ</p>
+                        </a>
+                      </div>
+                    </div>
+              <?php
+                endforeach;
+                wp_reset_postdata();
+                }
+              ?>
               <div class="c-banner_smail">
                 <div class="c-banner_btn">Marketing Strage</div>
                 <ul>
-                  <li><a href>
-                      <h3>5 Steps to Create an Outstanding Marketing Plan [Free Templates]</h3>
-                      <p>MARKETING | 15 MIN READ</p></a>
-                  </li>
+                  <?php if ( $myposts ) 
+                    { foreach ( $myposts as $post ) : setup_postdata( $post ); $i++; ?>
+                     <?php if ( $i >=2 && $i <=6 ) :?>
+                      <li>
+                        <a href="<?php the_permalink(); ?>">
+                          <h3><?php the_title(); ?></h3>
+                          <p><?php echo get_the_category()[0]->cat_name; ?>  | <?php the_field('time_read'); ?> MIN READ</p>
+                        </a>
+                      </li>
+                      <?php endif ; ?>
+                  <?php
+                    endforeach;
+                    wp_reset_postdata();
+                    }
+                    ?>
                 </ul>
               </div>
             </div>
