112dd4573057cb936213d5b56ecd2338d892bc4d
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-05-31 16:59:00 +0700

    tungnt

--- wp-content/themes/learnpress/functions.php
+++ wp-content/themes/learnpress/functions.php
@@ -265,6 +265,43 @@ function cptui_register_my_cpts() {
 	];
 
 	register_post_type( "ebook", $args );
+		/**
+	 * Post Type: Video.
+	 */
+
+	$labels = [
+		"name" => __( "Video", "custom-post-type-ui" ),
+		"singular_name" => __( "Video", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Video", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => false,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "postvideo", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "postvideo", $args );
+
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
@@ -356,3 +393,152 @@ add_action('wp_ajax_eload_more', 'eload_more');
 add_action('wp_ajax_nopriv_eload_more', 'eload_more');
 
 
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_628c3e150f8fa',
+		'title' => 'Download Ebook',
+		'fields' => array(
+			array(
+				'key' => 'field_628c3e2715358',
+				'label' => 'Link Ebook',
+				'name' => 'link_ebook',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'ebook',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+
+	acf_add_local_field_group(array(
+		'key' => 'group_628adf54aba9a',
+		'title' => 'Time_Read',
+		'fields' => array(
+			array(
+				'key' => 'field_628adf7c97bc2',
+				'label' => 'time_read',
+				'name' => 'time_read',
+				'type' => 'number',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => 15,
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'min' => '',
+				'max' => '',
+				'step' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+
+	acf_add_local_field_group(array(
+		'key' => 'group_6295e373e9053',
+		'title' => 'Video Url',
+		'fields' => array(
+			array(
+				'key' => 'field_6295e3991f9dd',
+				'label' => 'Video Url',
+				'name' => 'video_url',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 1,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'postvideo',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => array(
+			0 => 'the_content',
+			1 => 'excerpt',
+			2 => 'discussion',
+			3 => 'comments',
+			4 => 'revisions',
+			5 => 'author',
+			6 => 'format',
+			7 => 'page_attributes',
+			8 => 'categories',
+			9 => 'tags',
+			10 => 'send-trackbacks',
+		),
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+
+	endif;
--- wp-content/themes/learnpress/page-video.php
+++ wp-content/themes/learnpress/page-video.php
@@ -11,8 +11,62 @@
  *
  * @package learnpress
  */
-
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$video_list = get_posts( array(
+  'post_type' => array('postvideo'),
+  'post_status' => 'publish',
+  'posts_per_page' => 9,
+  'orderby'	=> 'post_date',
+	'order'         => 'DESC',
+  'paged'         => $paged
+) );
 get_header();?>
+      <main class="l-main">
+        <div class="c-learn blog blog_video">
+          <div class="l-container">
+            <div class="c-learn_inner" id="videoList">
+              <ul>
+
+                <?php if ( $video_list )
+                { foreach ( $video_list as $item ) : setup_postdata( $item ); ?>
+                <li class="c-learn_item" onclick="getVideoClick(this)" data-url="https://www.youtube.com/embed/9_V2q7C910o">
+                  <div class="c-learn_img">
+                  <img class="lazyload" loading="lazy" width="320" height="223"  data-src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($item->ID))[0] ?>" src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($item->ID))[0] ?>" alt="<?php echo($item->post_title)  ?>">
+                  </div>
+                  <div class="c-learn_content">
+                    <h4><?php echo($item->post_title)  ?></h4>
+                  </div>
+                </li>
+              <?php
+                endforeach;
+                wp_reset_postdata();
+                }
+              ?>
+
+              </ul>
+              <div class="modal" id="myModal">
+                <div class="modal-content">
+                  <iframe id="youtubeID" width="100%" height="400px" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <script>
+      function getVideoClick(element)
+      {
+         let VideoUrl = element.getAttribute("data-url");
+         document.getElementById('youtubeID').src=VideoUrl;
+         modal.style.display = "block";
+      }
+      var modal = document.getElementById("myModal");
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    </script>
 
 <?php
 //get_sidebar();
