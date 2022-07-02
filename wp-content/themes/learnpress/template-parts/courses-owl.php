8484328c77265907bb14e1bd4a911c829af52f0f
tungnt <nguyenthanhtung74119@gmail.com>
2022-07-02 07:56:42 +0700

    tungnt

--- wp-content/themes/learnpress/template-parts/courses-owl.php
+++ wp-content/themes/learnpress/template-parts/courses-owl.php
@@ -1,5 +1,5 @@
 <div class="owl-carousel owl-theme owl-course">
-          <div class="item"> <img src="<?php echo get_template_directory_uri();?>/assets/images/khoahoc.jpg" alt="" sizes="" srcset="">
+          <div class="item"> <img src="<?php echo get_template_directory_uri();?>/assets/images/khoahoc_category.jpg" alt="" sizes="" srcset="">
             <div class="content d-none"> <a href="http://">Completely Free & Online</a>
               <p class="ttl">All Courses and Certifications</p>
               <p class="des">Stay sharp . Learn the latest business trends from leading experts</p>
