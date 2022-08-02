8444bf890f938295ac460a5a5041136ee920c018
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-08-02 17:48:49 +0700

    tungnt

--- wp-content/themes/learnpress/functions/acf.php
+++ wp-content/themes/learnpress/functions/acf.php
@@ -255,4 +255,51 @@ if( function_exists('acf_add_local_field_group') ):
 		'description' => '',
 		'show_in_rest' => 0,
 	));
+
+	acf_add_local_field_group(array(
+		'key' => 'group_62e8c557d56b3',
+		'title' => 'Bài viết liên quan',
+		'fields' => array(
+			array(
+				'key' => 'field_62e8c557dae06',
+				'label' => 'Bài viết liên quan',
+				'name' => 'bai_viet_lien_quan',
+				'type' => 'post_object',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'post_type' => array(
+					0 => 'post',
+				),
+				'taxonomy' => '',
+				'allow_null' => 0,
+				'multiple' => 1,
+				'return_format' => 'id',
+				'ui' => 1,
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
 	endif;
