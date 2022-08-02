453ecb50ce03eac0abddf34bba74580d2dd0ba69
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-08-02 13:40:32 +0700

    tungnt

--- wp-content/themes/learnpress/template-parts/content-post.php
+++ wp-content/themes/learnpress/template-parts/content-post.php
@@ -30,4 +30,51 @@
             </div>
           </div>
         </div>
-      </main>
+        <div class="c-learn blog">
+          <div class="l-container">
+            <div class="c-learn_inner">
+              <ul class="publication-list courses-list">
+              <?php
+              if(get_field('bai_viet_lien_quan')){
+                $post_list = get_posts( array(
+                  'post_type' => array('post'),
+                  'post_status' => 'publish',
+                  'posts_per_page' => 3,
+                  'orderby'	=> 'post_date',
+                  'order'         => 'DESC',
+                  'post__in'=> get_field('bai_viet_lien_quan'),
+                ) );
+              }
+              else{
+                $post_list = get_posts( array(
+                  'post_type' => array('post'),
+                  'post_status' => 'publish',
+                  'posts_per_page' => 3,
+                  'orderby'	=> 'post_date',
+                  'order'         => 'DESC',
+                ) );
+              }
+              ?>
+              <?php if ( $post_list )
+                { foreach ( $post_list as $post_item ) : setup_postdata( $post_item ); ?>
+                    <li class="c-learn_item">
+                      <div class="c-learn_img">
+                        <img class="lazyload" width="320" height="223" loading="lazy" data-src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($post_item->ID), 'full')[0]; ?>" src="<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($post_item->ID), 'full')[0]; ?>" alt="<?php echo($post_item->post_title)  ?>"></div>
+                      <div class="c-learn_content">
+                        <a class="tag_a" href="<?php the_permalink($post_item->ID); ?>">
+                          <p class="c-learn_category"><?php echo get_the_category($post_item->ID)[0]->name; ?></p>
+                          <h4><?php echo($post_item->post_title)  ?></h4>
+                          <p class="c-learn_view"><?php echo getCrunchifyPostViews($post_item->ID); ?></p>
+                        </a>
+                      </div>
+                    </li>
+              <?php
+                endforeach;
+                wp_reset_postdata();
+                }
+              ?>
+              </ul>
+            </div>
+          </div>
+        </div>
+</main>
